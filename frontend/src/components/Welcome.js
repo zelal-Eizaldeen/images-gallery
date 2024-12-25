185a1436258b83e393beab453c1a5259aa9ab248
Zilal Eiz AlDin <zelalezaldeen@gmail.com>
2024-12-25 13:54:51 +0300

    Dockerfile for the frontend service

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Jumbotron, Button } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 
-const Welcome = () => (
-  <Jumbotron>
-    <h1>Syrian Embassy</h1>
-    <p>
-      This is simple application that retrieves passport requests. In order to
-      start enter any search term in the input
-    </p>
-    <p>
-      <Button variant="primary" href="https://unsplash.com" target="_blank">
-        Start
-      </Button>
-    </p>
-  </Jumbotron>
-);
+const Welcome = () => {
+  return (
+    <Container>
+      <h1>Syrian Embassy</h1>
+      <p>
+        This is simple application that retrieves passport requests. In order to
+        start enter any search term in the input
+      </p>
+      <p>
+        <Button variant="primary" href="https://unsplash.com" target="_blank">
+          Start
+        </Button>
+      </p>
+    </Container>
+  );
+};
 
 export default Welcome;
